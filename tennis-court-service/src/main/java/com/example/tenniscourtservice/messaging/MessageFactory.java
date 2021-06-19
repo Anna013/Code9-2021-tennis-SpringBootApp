1171e2567f96fd53965589814ac43367ac98a325
Anna013 <anjovanic13@gmail.com>
2021-06-19 20:58:57 +0200

    message update

--- tennis-court-service/src/main/java/com/example/tenniscourtservice/messaging/MessageFactory.java
+++ tennis-court-service/src/main/java/com/example/tenniscourtservice/messaging/MessageFactory.java
@@ -1,27 +1,25 @@
 package com.example.tenniscourtservice.messaging;
 
-import com.example.tenniscourtservice.tennis.TennisCourt;
+import com.example.tenniscourtservice.tennis.ReserveOperation;
+
+import com.example.tenniscourtservice.tennis.Timeslot;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.time.OffsetDateTime.now;
+import java.time.OffsetDateTime;
+
 
 @UtilityClass
 @Slf4j
 public class MessageFactory {
 
-    public static TennisCourtMessage createTennisCourtMessage(List<TennisCourt> court, String mail) {
-        return TennisCourtMessage.builder()
-                .email(mail)
-                .notificationType(NotificationType.EMAIL.name())
-                .courts(court.stream()
-                        .map(p -> p.getName() + ":" + p.getTimeslots())
-                        .collect(Collectors.toList()))
-                .sendingTime(now().toString())
-                .version(1).build();
-    }
-
-
+        public static TennisCourtMessage createMessage(Timeslot t, ReserveOperation operation) {
+            return TennisCourtMessage.builder()
+                    .typeOfOperation(operation.getText())
+                    .version(1)
+                    .notificationType(NotificationType.EMAIL.name())
+                    .sendingTime(OffsetDateTime.now().toString())
+                    .build();
+        }
 }
+
--- tennis-court-service/src/main/java/com/example/tenniscourtservice/messaging/TennisCourtMessage.java
+++ tennis-court-service/src/main/java/com/example/tenniscourtservice/messaging/TennisCourtMessage.java
@@ -2,6 +2,7 @@ package com.example.tenniscourtservice.messaging;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
@@ -15,8 +16,8 @@ public class TennisCourtMessage extends BaseMessage{
     @JsonProperty("email")
     String email;
 
-    @JsonProperty("courts")
-    List<String> courts;
+    @JsonProperty("type_of_operation")
+    String typeOfOperation;
 
     @JsonProperty("notification_type")
     String notificationType;
